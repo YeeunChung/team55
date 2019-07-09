2dc36c97f8aa6254f6bafc75e594d80a81989b56
YeeunChung <yesung00000@gmail.com>
2019-07-09 17:07:31 +0900

    grayscale reverse

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -8,7 +8,7 @@ file_list = os.listdir(path_dir)
 for input in file_list:
     imgread = './d/' + input
     print(imgread)
-    src = cv2.imread('./d/' + input, cv2.IMREAD_COLOR)
+    src = cv2.imread('./d/' + input, cv2.IMREAD_GRAYSCALE)
     dst = cv2.bitwise_not(src)
     output = Image.fromarray(dst)
     output.save("./dr/" + input)

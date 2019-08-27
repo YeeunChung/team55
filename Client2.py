0b96edf4b689fea6d6bcced5178ddd83ad759ef5
YeeunChung <yesung00000@gmail.com>
2019-08-27 17:02:44 +0900

    client-server2

--- Client2.py
+++ Client2.py
@@ -10,6 +10,7 @@ c.connect((HOST,12000))
 print ('ok')
 cnt=0
 while 1:
+    print("while")
     cnt+=1
     if (cnt%50==0):
         data = 'stop'
@@ -19,6 +20,7 @@ while 1:
         print(data)
         data = str.encode(data)
         c.send(data)
+        print("send data")
     else:
         continue
 c.close()
